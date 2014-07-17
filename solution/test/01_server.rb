a9fac498f28a190db342e83f6724ede40a3ddbb1
Ned Ruggeri <ruggeri@self-loop.com>
2014-07-17 16:44:01 -0700

    add 00_basic_server.rb

--- solution/test/01_server.rb
+++ solution/test/01_server.rb
@@ -2,10 +2,6 @@ require 'active_support/core_ext'
 require 'webrick'
 require_relative '../lib/rails_lite'
 
-# http://www.ruby-doc.org/stdlib-2.0/libdoc/webrick/rdoc/WEBrick.html
-# http://www.ruby-doc.org/stdlib-2.0/libdoc/webrick/rdoc/WEBrick/HTTPRequest.html
-# http://www.ruby-doc.org/stdlib-2.0/libdoc/webrick/rdoc/WEBrick/HTTPResponse.html
-# http://www.ruby-doc.org/stdlib-2.0/libdoc/webrick/rdoc/WEBrick/Cookie.html
 server = WEBrick::HTTPServer.new :Port => 8080
 trap('INT') { server.shutdown }
 
