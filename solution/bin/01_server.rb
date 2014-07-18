48a2bfa4a86d74df3f02fa602b97b712c87ac23e
Ned Ruggeri <ruggeri@self-loop.com>
2014-07-17 17:17:09 -0700

    prefer port 3000

--- solution/bin/01_server.rb
+++ solution/bin/01_server.rb
@@ -2,20 +2,24 @@ require 'active_support/core_ext'
 require 'webrick'
 require_relative '../lib/rails_lite'
 
-server = WEBrick::HTTPServer.new :Port => 8080
+server = WEBrick::HTTPServer.new(:Port => 3000)
 trap('INT') { server.shutdown }
 
 class MyController < ControllerBase
   def go
-    render_content("hello world!", "text/html")
+    if @req.path == "/cats"
+      render_content("hello cats!", "text/html")
+    else
+      redirect_to("/cats")
+    end
 
     # after you have template rendering, uncomment:
-#    render :show
+    # render :show
 
     # after you have sessions going, uncomment:
-#    session["count"] ||= 0
-#    session["count"] += 1
-#    render :counting_show
+    # session["count"] ||= 0
+    # session["count"] += 1
+    # render :counting_show
   end
 end
 
--- solution/bin/03_params_server.rb
+++ solution/bin/03_params_server.rb
@@ -7,7 +7,7 @@ require_relative '../lib/rails_lite'
 # http://www.ruby-doc.org/stdlib-2.0/libdoc/webrick/rdoc/WEBrick/HTTPRequest.html
 # http://www.ruby-doc.org/stdlib-2.0/libdoc/webrick/rdoc/WEBrick/HTTPResponse.html
 # http://www.ruby-doc.org/stdlib-2.0/libdoc/webrick/rdoc/WEBrick/Cookie.html
-server = WEBrick::HTTPServer.new :Port => 8080
+server = WEBrick::HTTPServer.new(:Port => 3000)
 trap('INT') { server.shutdown }
 
 class ExampleController < ControllerBase
